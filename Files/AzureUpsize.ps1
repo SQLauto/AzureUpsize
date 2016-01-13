6a44d0c14bdb6d9b04c8137cea4d65d1c251d53d
George Walkey <gwalkey@users.noreply.github.com>
2016-01-13 11:07:49 -0500

    Create AzureUpsize.ps1

---
+++ Files/AzureUpsize.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+   Creates an Azure Resource Group, Database Server, Storage Account, Firewall Rules and one Database
+	v
+.DESCRIPTION
+   Creates an Azure Resource Group, Database Server, Storage Account, Firewall Rules and one Database
+      
+.EXAMPLE
+    AzureGBW.ps1
+	
+
+.Inputs
+    
+
+.Outputs
+
+	
+.NOTES    
+	
+	George Walkey
+	Richmond, VA USA
+
+.LINK
+	https://github.com/gwalkey
+	
+#>
+
+# Get Powershell Here
+# https://azure.microsoft.com/en-us/documentation/articles/powershell-install-configure/
+
+if ((Get-Module -ListAvailable Azure) -eq $null) 
+{ 
+    throw "Windows Azure Powershell not found! Please install from https://azure.microsoft.com/en-us/documentation/articles/powershell-install-configure/" 
+} 
+Import-Module Azure
+
+Get-Module -ListAvailable Azure
